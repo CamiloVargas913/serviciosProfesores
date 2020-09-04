3ee28f96998b63bdd00270e9066f948c6a667adc
camilo <kmilovargas913@gmail.com>
2020-09-04 18:47:49 -0500

    registrar

--- Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/controller/ProfesoController.java
+++ Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/controller/ProfesoController.java
@@ -15,6 +15,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import co.edu.ucundinamarca.logica.ProfesorService;
+import co.edu.ucundinamarca.pojo.Profesor;
+import javax.validation.Valid;
 
 /**
  *
@@ -23,49 +26,57 @@ import javax.ws.rs.core.Response;
 @Stateless
 @Path("/profesores")
 public class ProfesoController {
+
     @Path("/insertar")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response insertarProfesor(){
-       return Response.status(Response.Status.CREATED).build();
+    public Response insertarProfesor(@Valid Profesor profesor) {    
+        System.out.println("info");
+        /*try {
+            ProfesorService profesoragregar = new ProfesorService();
+            profesoragregar.registroProfesor(profesor);
+            return Response.status(Response.Status.CREATED).entity("Insercion correcta").build();
+        } catch (Exception ex) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Error al insertar").build();
+        }*/
+        return Response.status(Response.Status.CREATED).build();
     }
-    
+
     @Path("/editar")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response editarProfesor(){
-       return Response.status(Response.Status.OK).build();
+    public Response editarProfesor() {
+        return Response.status(Response.Status.OK).build();
     }
-    
+
     @Path("/eliminar")
-    @DELETE     
+    @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    public Response elimiarProfesor(){
-       return Response.status(Response.Status.OK).build();
+    public Response elimiarProfesor() {
+        return Response.status(Response.Status.OK).build();
     }
-    
+
     @Path("/retornarProfesor")
-    @GET     
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listaProfesor(){
-       return Response.status(Response.Status.OK).build();
+    public Response listaProfesor() {
+        return Response.status(Response.Status.OK).build();
     }
-    
+
     @Path("/retornarProfesorCc")
-    @GET     
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listaProfesorCc(){
-       return Response.status(Response.Status.OK).build();
+    public Response listaProfesorCc() {
+        return Response.status(Response.Status.OK).build();
     }
-    
+
     @Path("/retornarProfesorMateria")
-    @GET     
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listaProfesorMateria(){
-       return Response.status(Response.Status.OK).build();
+    public Response listaProfesorMateria() {
+        return Response.status(Response.Status.OK).build();
     }
-    
-    
+
 }
--- Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/logica/ProfesorService.java
+++ Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/logica/ProfesorService.java
@@ -5,10 +5,36 @@
  */
 package co.edu.ucundinamarca.logica;
 
+import co.edu.ucundinamarca.pojo.Profesor;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author PROFESIONAL
  */
 public class ProfesorService {
+    List<Profesor> ListaProfesores;
+    public void registroProfesor(Profesor profesor){
+        this.ListaProfesores.add(profesor);
+        this.llenarProfesor();
+    }
     
+    public Response llenarProfesor() {
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream("bd.txt");
+            ObjectOutputStream db = new ObjectOutputStream(fos);
+            db.writeObject(this.ListaProfesores);
+            db.flush();
+            db.close();
+            return Response.status(Response.Status.CREATED).entity("Insercion correcta").build();
+        } catch (IOException ex) {
+           return Response.status(Response.Status.BAD_REQUEST).entity("Error al insertar").build();
+        }
+    }
 }

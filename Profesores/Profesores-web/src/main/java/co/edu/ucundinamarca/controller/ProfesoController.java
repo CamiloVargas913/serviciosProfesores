c6fdefad09aa2f10e1cd033fae16f4832eef5728
David Marquez <d>
2020-09-06 19:26:32 -0500

    Servicios de listar por cedula, por materias, todos y eliminar

--- Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/controller/ProfesoController.java
+++ Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/controller/ProfesoController.java
@@ -17,7 +17,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import co.edu.ucundinamarca.logica.ProfesorService;
 import co.edu.ucundinamarca.pojo.Profesor;
+import java.util.List;
 import javax.validation.Valid;
+import javax.ws.rs.PathParam;
 
 /**
  *
@@ -31,7 +33,7 @@ public class ProfesoController {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response insertarProfesor(@Valid Profesor profesor) {    
+    public Response insertarProfesor(@Valid Profesor profesor) {
         try {
             ProfesorService profesoragregar = new ProfesorService();
             profesoragregar.registroProfesor(profesor);
@@ -50,32 +52,57 @@ public class ProfesoController {
         return Response.status(Response.Status.OK).build();
     }
 
-    @Path("/eliminar")
+    @Path("/eliminar/{id}")
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    public Response elimiarProfesor() {
-        return Response.status(Response.Status.OK).build();
+    public Response eliminarProfesor(@PathParam("id") int id) {
+        ProfesorService profesor = new ProfesorService();
+        String data = profesor.eliminarProfesor(id);
+        if (data.equals("El id no existe")) {
+            return Response.status(Response.Status.NO_CONTENT).entity("No existe el id").build();
+        } else {
+            return Response.status(Response.Status.OK).entity(data).build();
+        }
+
     }
 
     @Path("/retornarProfesor")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response listaProfesor() {
-        return Response.status(Response.Status.OK).build();
+        ProfesorService profesor = new ProfesorService();
+        List<Profesor> dataProfesor = profesor.retornarProfesores();
+        if (dataProfesor != null) {
+            return Response.status(Response.Status.OK).entity(dataProfesor).build();
+        } else {
+            return Response.status(Response.Status.NO_CONTENT).entity("No hay registros").build();
+        }
     }
 
-    @Path("/retornarProfesorCc")
+    @Path("/retornarProfesorCc/{cedula}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listaProfesorCc() {
-        return Response.status(Response.Status.OK).build();
+    public Response listaProfesorCc(@PathParam("cedula") String cedula) {
+        ProfesorService profesor = new ProfesorService();
+        Profesor dataProfesor = profesor.retornarProfesorCedula(cedula);
+        if (dataProfesor != null) {
+            return Response.status(Response.Status.OK).entity(dataProfesor).build();
+        } else {
+            return Response.status(Response.Status.NO_CONTENT).entity("No hay registros").build();
+        }
     }
 
-    @Path("/retornarProfesorMateria")
+    @Path("/retornarProfesorMateria/{materia}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listaProfesorMateria() {
-        return Response.status(Response.Status.OK).build();
+    public Response listaProfesorMateria(@PathParam("materia") String materia) {
+        ProfesorService profesor = new ProfesorService();
+        List<Profesor> dataProfesor = profesor.retornarProfesorMateria(materia);
+        if (dataProfesor != null) {
+            return Response.status(Response.Status.OK).entity(dataProfesor).build();
+        } else {
+            return Response.status(Response.Status.NO_CONTENT).entity("No hay registros").build();
+        }
     }
 
 }
--- Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/logica/ProfesorService.java
+++ Profesores/Profesores-web/src/main/java/co/edu/ucundinamarca/logica/ProfesorService.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,22 +24,25 @@ import javax.ws.rs.core.Response;
  * @author PROFESIONAL
  */
 public class ProfesorService {
-    List<Profesor> ListaProfesores = new ArrayList(); 
-    public void registroProfesor(Profesor profesor){
-        if(this.leer() != null){
+
+    List<Profesor> ListaProfesores = new ArrayList();
+    Profesor profesor = new Profesor();
+
+    public void registroProfesor(Profesor profesor) {
+        if (this.leer() != null) {
             this.ListaProfesores = this.leer();
-        }    
+        }
         this.ListaProfesores.add(profesor);
         this.llenarProfesor();
         for (Profesor ListaProfesore : ListaProfesores) {
-                System.out.println(ListaProfesore.getApellido());
-            }
+            System.out.println(ListaProfesore.getApellido());
+        }
     }
-    
+
     public void llenarProfesor() {
         FileOutputStream fos;
         try {
-            fos = new FileOutputStream("E:\\archivo.txt");
+            fos = new FileOutputStream("C:\\Users\\david\\Documents\\VIII Semestre\\Linea II\\serviciosProfesores\\archivo.txt");
             ObjectOutputStream db = new ObjectOutputStream(fos);
             db.writeObject(this.ListaProfesores);
             db.flush();
@@ -47,14 +51,96 @@ public class ProfesorService {
             System.out.println(ex);
         }
     }
+
     public List<Profesor> leer() {
         FileInputStream fis;
         try {
-            fis = new FileInputStream("E:\\archivo.txt");
+            fis = new FileInputStream("C:\\Users\\david\\Documents\\VIII Semestre\\Linea II\\serviciosProfesores\\archivo.txt");
             ObjectInputStream ois = new ObjectInputStream(fis);
             return (List<Profesor>) ois.readObject();
         } catch (IOException | ClassNotFoundException ex) {
             return null;
+        }
+    }
+
+    public List<Profesor> retornarProfesores() {
+        if (this.leer() != null) {
+            this.ListaProfesores = this.leer();
+            return this.ListaProfesores;
+        } else {
+            return null;
+        }
+    }
+
+    public Profesor retornarProfesorCedula(String cedula) {
+        if (this.leer() != null) {
+            boolean bandera = true;
+            this.ListaProfesores = this.leer();
+            for (Profesor ListaProfesore : ListaProfesores) {
+                if (ListaProfesore.getCedula().equals(cedula)) {
+                    this.profesor = ListaProfesore;
+                    return this.profesor;
+                } else {
+                    bandera = false;
+                }
+            }
+            if (bandera) {
+                return this.profesor;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public String eliminarProfesor(int id) {
+        if (this.leer() != null) {
+            this.ListaProfesores = this.leer();
+            boolean bandera = true;
+            Iterator<Profesor> iterator = this.ListaProfesores.iterator();
+            while (iterator.hasNext()) {
+                Profesor datos = iterator.next();
+                if (id == datos.getId()) {
+                    iterator.remove();
+                    bandera = true;
+                } else {
+                    bandera = false;
+                }
+            }
+            for (Profesor ListaProfe : ListaProfesores) {
+                System.out.println(ListaProfe.getNombre());
+            }
+            if (bandera) {
+                this.llenarProfesor();
+                return "Eliminado correctamente";
+            } else {
+                return "El id no existe";
+            }
+        } else {
+            return "No hay ningun profesor registrado";
+        }
+
+    }
+
+    public List<Profesor> retornarProfesorMateria(String materia) {
+        List<Profesor> profesores = new ArrayList();
+        if (this.leer() != null) {
+            this.ListaProfesores = this.leer();
+            for (Profesor ListaProfesor : ListaProfesores) {
+                for (String listaMateria : ListaProfesor.getListaMateria()) {
+                    if (listaMateria.contains(materia)) {
+                        profesores.add(ListaProfesor);
+                    }
+                }
+            }
+        } else {
+            return null;
+        }
+        if (profesores.size() <= 0) {
+            return null;
+        } else {
+            return profesores;
         }
     }
 }

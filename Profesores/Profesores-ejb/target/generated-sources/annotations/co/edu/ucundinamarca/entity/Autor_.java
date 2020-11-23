f2360923fdd80ccef4856f3ed7ebd7394e3195c2
camilo <kmilovargas913@gmail.com>
2020-11-23 10:19:24 -0500

    web

--- Profesores/Profesores-ejb/target/generated-sources/annotations/co/edu/ucundinamarca/entity/Autor_.java
+++ Profesores/Profesores-ejb/target/generated-sources/annotations/co/edu/ucundinamarca/entity/Autor_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-20T23:08:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-23T09:27:50")
 @StaticMetamodel(Autor.class)
 public class Autor_ { 
 
--- Profesores/Profesores-ejb/target/generated-sources/annotations/co/edu/ucundinamarca/entity/Lector_.java
+++ Profesores/Profesores-ejb/target/generated-sources/annotations/co/edu/ucundinamarca/entity/Lector_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-20T23:08:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-23T09:27:50")
 @StaticMetamodel(Lector.class)
 public class Lector_ { 
 
